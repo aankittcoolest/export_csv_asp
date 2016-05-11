068aa94e12a946cd3f4cf171d21cd95160e05bd1
aankittcoolest <aankittcoolest@gmail.com>
2016-05-11 09:56:05 +0900

    Export data to csv

---
+++ WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebForm1.aspx.cs" Inherits="WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:DropDownList ID="ddlExportFormat" runat="server">
+            <asp:ListItem Text="COMMA DELIMITED" Value="COMMA DELIMETED"></asp:ListItem>
+            <asp:ListItem Text="PIPE DELIMITED" Value="PIPE DELIMETED"></asp:ListItem>
+        </asp:DropDownList>
+    
+    </div>
+        <asp:Button ID="btnExport" runat="server" Text="Export" OnClick="btnExport_Click" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text;
+using System.Data;
+using System.IO;
+
+public partial class WebForm1 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnExport_Click(object sender, EventArgs e)
+    {
+        string strDelimeter = ddlExportFormat.SelectedValue == "COMMA DELIMITED" ? "," : "|";
+        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        StringBuilder sb = new StringBuilder();
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = new SqlCommand("SELECT [ID],[Name],[Location] FROM [dbo].[Departments];SELECT [ID],[Name],[DepartmentID] FROM [dbo].[Employees];", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            ds.Tables[0].TableName = "Departments";
+            ds.Tables[1].TableName = "Employees";
+
+            foreach (DataRow departmentDR in ds.Tables["Departments"].Rows)
+            {
+                int departmentId = Convert.ToInt32(departmentDR["ID"]);
+                sb.Append(departmentId.ToString() + strDelimeter);
+                sb.Append(departmentDR["Name"].ToString() + strDelimeter);
+                sb.Append(departmentDR["Location"].ToString());
+                sb.Append("\r\n");
+
+                foreach (DataRow employeeDR in ds.Tables["Employees"].Select("DepartmentId = '" + departmentId.ToString() + "'"))
+                {
+                    sb.Append(employeeDR["ID"].ToString() + strDelimeter);
+                    sb.Append(employeeDR["Name"].ToString() + strDelimeter);
+                    sb.Append(departmentId.ToString());
+                    sb.Append("\r\n");
+                }
+
+            }
+        }
+
+        //string strFileName = strDelimeter == "," ? "Data.csv" : "Data.txt";
+        string strFileName = "Data.csv";
+
+        StreamWriter file = new StreamWriter(@"C:\Users\Ankit\OneDrive\Documents\ExportedData\" + strFileName);
+        file.WriteLine(sb.ToString());
+        file.Close();
+    }
+
+    
+}
